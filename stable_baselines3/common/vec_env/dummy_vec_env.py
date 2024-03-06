86fb94d342db10593a6c044bfeb86c8e1f90730c
Patrick Hopf <asdf>
2024-03-06 16:21:54 +0000

    fix training without circuit

--- stable_baselines3/common/vec_env/dummy_vec_env.py
+++ stable_baselines3/common/vec_env/dummy_vec_env.py
@@ -44,7 +44,8 @@ class DummyVecEnv(VecEnv):
         self.keys, shapes, dtypes = obs_space_info(obs_space)
 
         self.buf_obs = OrderedDict([(k, np.zeros((self.num_envs, *tuple(shapes[k])), dtype=dtypes[k])) for k in self.keys])
-        self.buf_obs["circuit"] = [None for _ in range(self.num_envs)]
+        if "circuit" in self.keys:
+            self.buf_obs["circuit"] = [None for _ in range(self.num_envs)]
         self.buf_dones = np.zeros((self.num_envs,), dtype=bool)
         self.buf_rews = np.zeros((self.num_envs,), dtype=np.float32)
         self.buf_infos: List[Dict[str, Any]] = [{} for _ in range(self.num_envs)]
